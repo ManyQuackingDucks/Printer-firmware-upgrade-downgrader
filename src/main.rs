06dca6acc44e67f6b76194dfc560a6258f5dfbee
A Duck <36427583+ManyQuackingDucks@users.noreply.github.com>
2022-02-14 23:45:39 +0000

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use ftp::FtpStream as Ftpcmd;
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+use ftp::FtpStream as FtpServer;
+use std::fs::File;
 use std::io;
-#[derive(Debug)]
+use std::io::Read;
 struct UpdateData {
     supported_models: Vec<String>,
     date_code: String,
     supported_langs: String,
 }
 fn main() {
-    println!("This utility will revert your HP printer to a version that supports cartriges.");
-    println!("Please enter your printers ip: ");
     match send_update() {
         Ok(_) => println!("Success. Your printer should say \"PROGRAMING\""),
         Err(e) => eprintln!("{e}"),
@@ -16,40 +17,60 @@ fn main() {
 }
 
 fn send_update() -> Result<(), String> {
-    let file = include_bytes!("update.rfu");
-    println!("{:?}", parse_file(file)?);
-    let mut buffer = String::new();
     let stdin = io::stdin();
+    println!("Please enter the location of the update file");
+    let mut file_location = String::new();
+    stdin
+        .read_line(&mut file_location)
+        .map_err(|e| format!("Could not read from stdin. [{e}]"))?;
+    file_location = file_location.trim().to_string();
+    let mut f = File::open(&file_location)
+        .map_err(|e| format!("Could not open file: {} [{e}]", &file_location))?;
+    // read up to 10 kilabytes (the first PJL section plus some incase rfu files are diferent)
+    let mut buffer = [0; 10000];
+
+    f.read(&mut buffer)
+        .map_err(|e| format!("Could not read from file {} [{e}]", &file_location))?;
+    let update_data = parse_rfu_bytes(&buffer)?;
+
+    println!("Loaded file: {}\nSupported models:", &file_location);
+    for i in update_data.supported_models {
+        println!("{i}");
+    }
+    println!(
+        "datecode: {}\nSupported langs: {}",
+        update_data.date_code, update_data.supported_langs
+    );
+    println!("Please enter your printers ip: ");
+    let mut buffer = String::new();
     stdin
         .read_line(&mut buffer)
         .map_err(|e| format!("Could not read from stdin. [{e}]"))?;
-    let mut conn = Ftpcmd::connect(buffer.trim().to_owned() + ":21")
+    let mut conn = FtpServer::connect(buffer.trim().to_string() + ":21")
         .map_err(|e| format!("Could not connect to printer. Is tftp enabled? [{e}]"))?;
-    conn.put("update.rfu", &mut std::io::Cursor::new(file))
+    conn.put("update.rfu", &mut f)
         .map_err(|e| format!("Could not send file to printer. Try again. [{e}]"))?;
     conn.quit()
         .map_err(|e| format!("Could not close connection succesfully. [{e}]"))?;
     Ok(())
 }
 
-fn parse_file(file: &[u8]) -> Result<UpdateData, String> {
+///Returns some information about the update file
+fn parse_rfu_bytes(file: &[u8]) -> Result<UpdateData, String> {
     let string = String::from_utf8_lossy(file);
-    let split_char = "%-12345X";//Known as the PJL Universal Exit Charecter
+    let split_char = "\u{001B}%-12345X"; //<ESC>%-12345X Known as the PJL Universal Exit Charecter
     
     //This is probabaly inefficent and looks bad. We should find a way to make it look better or at least more efficent.
     let info_parts = string
         .split_once(split_char)
         .unwrap()
-        .1
+        .1 //Enter PJL
         .split_once(split_char)
         .unwrap()
-        .0;
-    let mut vec_data: Vec<&str> = info_parts
+        .0; //Exit PJL
+    let vec_data: Vec<&str> = info_parts
         .lines()
         .filter(|x| x.starts_with("@PJL COMMENT "))
-        .collect();
-    vec_data = vec_data
-        .iter()
         .map(|x| x.strip_prefix("@PJL COMMENT ").unwrap())
         .collect();
     let mut date_code = String::new();
@@ -58,17 +79,17 @@ fn parse_file(file: &[u8]) -> Result<UpdateData, String> {
     for i in vec_data {
         let (first, second) = i.split_once('=').unwrap();
         match first {
-            "LOCVERSION" => (),
+            "LOCVERSION" |
             "VERSION" => (),
             "DATECODE" => date_code = second.to_string(),
             "MODEL" => supported_models.push(second.to_string()),
             "LANGUAGEGROUP" => supported_langs = second.to_string(),
-            _ => return Err(format!("Unexpected rfu comment value {first}")),
+            _ => return Err(format!("Unexpected PJL comment value {first}")),
         };
     }
     Ok(UpdateData {
         supported_models,
-        supported_langs,
         date_code,
+        supported_langs,
     })
 }
